3f08784bc6abff24e53884acdeef59ae5dd8ce22
kkkunnnnethan <ethanwu1127@gmail.com>
2022-01-25 11:26:18 +0800

    fix: fix bugs in data_prepare.py

--- data_prepare.py
+++ data_prepare.py
@@ -38,9 +38,9 @@ def rewrite_label(annos, dst_folder, is_plane = False):
             with open(file, 'r') as f:
                 _lines = f.readlines()
                 if is_plane:
-                    lines = ['airplane  ' + x for x in _lines]  
+                    lines = ['airplane\t' + x for x in _lines]  
                 else:
-                    lines = ['car  ' + x for x in _lines]  
+                    lines = ['car\t' + x for x in _lines]  
                 content = ''.join(lines)
                 fw.write(content)
 
@@ -59,10 +59,13 @@ def creat_tree(root_dir):
     rewrite_label(airplane_annos, os.path.join(root_dir, 'Annotations'), True)
 
 
-def generate_test(root_dir):
-    setfile = os.path.join(root_dir, 'ImageSets/test.txt')
+def generate_files(root_dir, type):
+    if type not in ["train", "test", "val"]:
+        assert False, "wrong type"
+    setfile = os.path.join(root_dir, 'ImageSets/{}.txt'.format(type))
     img_dir = os.path.join(root_dir, 'AllImages')
-    test_dir = os.path.join(root_dir, 'Test')
+    label_dir = os.path.join(root_dir, 'Annotations')
+    test_dir = os.path.join(root_dir, type)
     os.makedirs(test_dir)
     if not os.path.exists(setfile):
         raise RuntimeError('{} is not founded!'.format(setfile))
@@ -70,40 +73,20 @@ def generate_test(root_dir):
         lines = f.readlines()
         pbar = tqdm(lines)
         for line in pbar:
-            pbar.set_description("Copying to Test dir...")
+            pbar.set_description("Copying to {} dir...".format(type))
             filename = line.strip()
             src = os.path.join(img_dir, filename + '.png')
             dst = os.path.join(test_dir, filename + '.png')
             shutil.copyfile(src, dst)
-
-# DON'T run this function, casuse you cannot make sure the same result will be generated.
-# def generate_imageset_file(root_dir):
-#     split_ratio=[0.5, 0.2, 0.3]
-#     train_r, val_r, test_r = split_ratio
-#     car = set([x for x in range(1,511)])
-#     airplane = set([x for x in range(511, 1511)])
-#     train_car = set(random.sample(car,int(510 * train_r)))
-#     val_car = set(random.sample(car - train_car,int(510 * val_r)))
-#     test_car = car - val_car - train_car
-#     train_airplane = set(random.sample(airplane,int(1000 * train_r)))
-#     val_airplane = set(random.sample(airplane - train_airplane,int(1000 * val_r)))
-#     test_airplane = airplane - val_airplane - train_airplane
-#     train_idx = train_car | train_airplane
-#     val_idx = val_car | val_airplane
-#     test_idx = test_car | test_airplane
-#     with open(os.path.join(root_dir, 'ImageSets/train.txt'), 'w') as f1:
-#         s1 = '\n'.join(['P' + str(x).zfill(4) for x in train_idx])
-#         f1.write(s1)
-#     with open(os.path.join(root_dir, 'ImageSets/val.txt'), 'w') as f2:
-#         s2 = '\n'.join(['P' + (str(x)).zfill(4) for x in val_idx])
-#         f2.write(s2)
-#     with open(os.path.join(root_dir, 'ImageSets/test.txt'), 'w') as f3:
-#         s3 = '\n'.join(['P' + str(x).zfill(4) for x in test_idx])
-#         f3.write(s3)
+            src = os.path.join(label_dir, filename + '.txt')
+            dst = os.path.join(test_dir, filename + '.txt')
+            shutil.copyfile(src, dst)
 
 
 if __name__ == "__main__":
-    root_dir = '/data-input/das_dota/UCAS_AOD'
+    root_dir = 'UCAS_AOD/'
     creat_tree(root_dir)
-    generate_test(root_dir)
+    generate_files(root_dir, "train")
+    generate_files(root_dir, "test")
+    generate_files(root_dir, "val")
 
